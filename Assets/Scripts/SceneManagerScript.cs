c1eb4205ef7e4fed2e824c410b4d27b6c7eb1b50
angelabytes <89052245+angelabytes@users.noreply.github.com>
2024-10-29 20:32:05 -1000

    added player profile and scene navigation

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void UnloadScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    public void LoadSceneAsync(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
