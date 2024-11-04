1840ab012a32c99c28a5a25f47b432713e33c707
Alex <alex@bayze.com>
2024-11-04 13:31:54 -1000

    Add animated Pigeon Flock asset in AR Scene

---
+++ Assets/Pigeon/Scripts/PigeonUserControllerScript3.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PigeonUserControllerScript3 : MonoBehaviour {
+	public PigeonCharacterScript3 pigeonCharacter;
+	void Start () {
+		pigeonCharacter = GetComponent<PigeonCharacterScript3> ();	
+	}
+	
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.L)) {
+			pigeonCharacter.Landing();
+		}
+		
+		if (Input.GetButtonDown ("Jump")) {
+			pigeonCharacter.Soar ();
+		}
+		
+		if (Input.GetButtonDown ("Fire1")) {
+			pigeonCharacter.Attack ();
+		}
+		
+		if (Input.GetKeyDown(KeyCode.E)) {
+			pigeonCharacter.Eat();
+		}
+
+		if (Input.GetKeyDown(KeyCode.H)) {
+			pigeonCharacter.Hop();
+		}		
+	}
+	
+	void FixedUpdate(){
+		pigeonCharacter.SetForwardAcceleration(Input.GetAxis ("Vertical"));
+		pigeonCharacter.turnSpeed=Input.GetAxis ("Horizontal");
+	}
+	
+}
