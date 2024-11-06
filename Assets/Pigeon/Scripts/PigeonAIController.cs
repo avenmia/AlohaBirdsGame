f5ebfb77017ed65a31ee70c7727962f2aeb35271
Alex <113944962+Alexandra-Haynes@users.noreply.github.com>
2024-11-05 18:49:46 -0800

    Add animated Pigeon Flock asset in AR Scene (#40)

---
+++ Assets/Pigeon/Scripts/PigeonAIController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PigeonAIController : MonoBehaviour {
+	public PigeonCharacterScript3 pigeonCharacter;
+
+	void Start () {
+		pigeonCharacter = GetComponent<PigeonCharacterScript3> ();	
+	}
+
+	void SeekFood(){
+		Vector3 targetRelPos = pigeonCharacter.food.transform.position - transform.position;
+		if (targetRelPos.sqrMagnitude < 5f && pigeonCharacter.isFlying && !pigeonCharacter.soaring) {
+			pigeonCharacter.tryingToLand = true;
+			pigeonCharacter.SetForwardAcceleration (-1f);
+			pigeonCharacter.SetUpSpeed (-1f);
+		} else if (targetRelPos.sqrMagnitude > 30f && !pigeonCharacter.isFlying && !pigeonCharacter.tryingToLand) {
+			pigeonCharacter.Soar ();
+		} else if (targetRelPos.sqrMagnitude < .083f) {
+			pigeonCharacter.SetForwardAcceleration(0f);
+			pigeonCharacter.Eat();
+		}else{
+			pigeonCharacter.SetForwardAcceleration(.5f);
+		}
+		targetRelPos.Normalize();
+		pigeonCharacter.turnSpeed = -Vector3.Dot (targetRelPos,transform.forward);
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.GetComponent<PigeonCharacterScript3>()){
+			pigeonCharacter.Soar();
+		}
+	}
+
+	void FixedUpdate(){
+		SeekFood ();
+	}
+}
