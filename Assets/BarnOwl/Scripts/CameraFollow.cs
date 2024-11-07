0d704aaf7c96d13d65099a096cae26c77d58942e
Alex <alex@bayze.com>
2024-11-06 15:19:00 -1000

    add animated barn owl asset

---
+++ Assets/BarnOwl/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 offset;
+    public Transform target;
+    public float speedSmooth = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 DesiredPosition = target.position + offset;
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, DesiredPosition, speedSmooth * Time.deltaTime);
+        transform.position = SmoothedPosition;
+
+        transform.LookAt(target);
+	}
+}
