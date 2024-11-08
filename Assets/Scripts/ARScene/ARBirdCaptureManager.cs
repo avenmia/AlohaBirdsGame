1dc929e6d78159ab2ebbbf28692fe85e7b8a4694
angelabytes <89052245+angelabytes@users.noreply.github.com>
2024-11-08 12:35:13 -1000

    updated the capture scene

--- Assets/Scripts/ARScene/ARBirdCaptureManager.cs
+++ Assets/Scripts/ARScene/ARBirdCaptureManager.cs
@@ -11,12 +11,13 @@ public class ARBirdCaptureManager : MonoBehaviour
     public GameObject imagePrefab;
     public GameObject popupPrefab;
     public Canvas mainCanvas;
+    private bool isBirdCaptured = false;
+    private bool isScreenTransitioning = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //For testing purposes can be deleted
-        //ShowPopup("You captured a pigeon");
+
     }
 
     void Update()
@@ -24,7 +25,7 @@ public class ARBirdCaptureManager : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Began && !isBirdCaptured && !isScreenTransitioning)
             {
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit;
@@ -33,10 +34,15 @@ public class ARBirdCaptureManager : MonoBehaviour
                     if (hit.collider.CompareTag("Bird"))
                     {
                         CaptureBird(hit.collider.gameObject);
-
+                        isBirdCaptured = true;
+                        return;
                     }
                 }
             }
+        }
+        else if(Input.touchCount == 0)
+        {
+            isBirdCaptured = false;
         }
     }
 
@@ -66,6 +72,8 @@ public class ARBirdCaptureManager : MonoBehaviour
         }
 
         ShowPopup($"You captured a {birdSpawnData.birdName}");
+        isScreenTransitioning = true;
+        StartCoroutine(ReturnToMap(6f));
         
         // TODO: Uncomment to save screenshot 
         // StartCoroutine(SaveScreenshotToGallery());
@@ -79,13 +87,20 @@ public class ARBirdCaptureManager : MonoBehaviour
             GameObject popup = Instantiate(popupPrefab, mainCanvas.transform);
             popup.GetComponentInChildren<TMP_Text>().text = message;
             // Optionally, add animations or auto-destroy after some time
-            Destroy(popup, 3f); // Destroys the popup after 2 seconds
+            Destroy(popup, 5f); // Destroys the popup after 2 seconds
 
         }
         else
         {
             Debug.LogWarning("Popup Prefab is not assigned.");
         }
+    }
+
+    private IEnumerator ReturnToMap(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        NavigationManager.Instance.ReturnToPrevScene();
+        isScreenTransitioning = false;
     }
 
     IEnumerator SaveScreenshotToGallery()
