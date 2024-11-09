00d54f90f32d4df09fddaad92eed7f9088723bd6
Angela <89052245+angelabytes@users.noreply.github.com>
2024-11-08 14:04:10 -1000

    Capture bird scene (#48)
    * animated text for BirdCapturePopup
    
    * updated
    
    * updated the capture scene
    
    * added profile settings
    
    * Update MapsAuthConfig.asset
    
    * Update ProjectSettings.asset
    
    Removing merge conflict
    
    * Update ProjectSettings.asset
    
    Adding settings back
    
    ---------
    
    Co-authored-by: avenmia <mavend2@gmail.com>

--- Assets/Scripts/ARScene/ARBirdCaptureManager.cs
+++ Assets/Scripts/ARScene/ARBirdCaptureManager.cs
@@ -12,12 +12,13 @@ public class ARBirdCaptureManager : MonoBehaviour
     public GameObject imagePrefab;
     public GameObject popupPrefab;
     public Canvas mainCanvas;
+    private bool isBirdCaptured = false;
+    private bool isScreenTransitioning = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //For testing purposes can be deleted
-        //ShowPopup("You captured a pigeon");
+
     }
 
     void Update()
@@ -25,7 +26,7 @@ public class ARBirdCaptureManager : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Began && !isBirdCaptured && !isScreenTransitioning)
             {
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit;
@@ -34,10 +35,15 @@ public class ARBirdCaptureManager : MonoBehaviour
                     if (hit.collider.CompareTag("Bird"))
                     {
                         CaptureBird(hit.collider.gameObject);
-
+                        isBirdCaptured = true;
+                        return;
                     }
                 }
             }
+        }
+        else if(Input.touchCount == 0)
+        {
+            isBirdCaptured = false;
         }
     }
 
@@ -70,6 +76,8 @@ public class ARBirdCaptureManager : MonoBehaviour
         }
 
         ShowPopup($"You captured a {birdSpawnData.birdName}");
+        isScreenTransitioning = true;
+        StartCoroutine(ReturnToMap(6f));
         
         // TODO: Uncomment to save screenshot 
         StartCoroutine(SaveScreenshotToGallery(birdSpawnData.birdName));
@@ -83,13 +91,19 @@ public class ARBirdCaptureManager : MonoBehaviour
             GameObject popup = Instantiate(popupPrefab, mainCanvas.transform);
             popup.GetComponentInChildren<TMP_Text>().text = message;
             // Optionally, add animations or auto-destroy after some time
-            Destroy(popup, 3f); // Destroys the popup after 2 seconds
-
+            Destroy(popup, 5f); // Destroys the popup after 2 seconds
         }
         else
         {
             Debug.LogWarning("Popup Prefab is not assigned.");
         }
+    }
+
+    private IEnumerator ReturnToMap(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        NavigationManager.Instance.ReturnToPrevScene();
+        isScreenTransitioning = false;
     }
 
     IEnumerator SaveScreenshotToGallery(string birdName)
