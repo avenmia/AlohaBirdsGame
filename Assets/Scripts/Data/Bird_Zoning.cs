04bc643149813754ffb82c51136fb1bf3919fda6
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2025-03-08 09:52:43 -1000

    update to zoning

--- Assets/Scripts/Data/Bird_Zoning.cs
+++ Assets/Scripts/Data/Bird_Zoning.cs
@@ -1,47 +1,59 @@
 using DataBank;
 using Niantic.Lightship.Maps;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Bird_Zoning : MonoBehaviour
 {
     [SerializeField] private LightshipMapView LMV;
     //List<string> Bird_Entities = new List<string>();
-    //public string Birds = "";
     [SerializeField] private TMP_Text Birds;
+    [SerializeField] private List<string> Bird_Names;
     void Start()
     {
-        Populate_List();
+        StartCoroutine(Bird_Location_Ping(0));
     }
 
     public void Populate_List()
     {
-        Birds.text = "";
         BirdDb mBirdDb = new BirdDb();
-        Debug.Log("Lat: " + 21.31624 + "\nLng: " + -157.858102);
-        System.Data.IDataReader reader 
+        //Debug.Log("Lat: " + 21.31624 + "\nLng: " + -157.858102);
+        Vector2 playerLocation = new Vector2(Input.location.lastData.latitude, Input.location.lastData.longitude);
+        //System.Data.IDataReader reader 
+        //    = mBirdDb.Close_Birds(playerLocation.x, playerLocation.y);
+        System.Data.IDataReader reader
             = mBirdDb.Close_Birds((float)21.31624, (float)-157.858102);
 
         int fieldCount = reader.FieldCount;
-        //List<BirdEntity> myList = new List<BirdEntity>();
         while (reader.Read())
         {
             //lat, lng, species
-            Debug.Log(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[1].ToString());
+            //Debug.Log(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[1].ToString());
             Birds.text += reader[2].ToString();
-            //BirdEntity entity = new BirdEntity(reader[0].ToString(),
-            //                                            reader[1].ToString(),
-            //                                            reader[2].ToString());
-            //Debug.Log(entity._Species);
-            //myList.Add(entity);
+            string[] words = reader[2].ToString().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            foreach(var bird in words)
+            {
+                //once we have a dictionary of all birds we have only put in viable bird names
+                Bird_Names.Add(bird);
+            }
         }
     }
 
-    IEnumerator Bird_Location_Ping()
+    IEnumerator Spawn_Viable_Bird(int seconds)
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(seconds);
+        var randomNum = UnityEngine.Random.Range(0, Bird_Names.Capacity);
+        //Instantiate by string
+    }
+
+    IEnumerator Bird_Location_Ping(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
         Populate_List();
+        StartCoroutine(Bird_Location_Ping(30));
     }
 }
