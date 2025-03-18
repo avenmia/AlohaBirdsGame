4a267ab94201e0ea15940037eed0817bcb847d89
avenmia <mavend2@gmail.com>
2025-03-17 16:26:22 -1000

    Fixing honey creeper asset

--- Assets/Scripts/Data/BirdTypeUtil.cs
+++ Assets/Scripts/Data/BirdTypeUtil.cs
@@ -10,6 +10,10 @@ public static class BirdTypeUtil
             case "Barn Owl": return BirdType.BarnOwl;
             case "African Silverbill": return BirdType.AfricanSilverbill;
             case "House Sparrow": return BirdType.HouseSparrow;
+            case "Hawaiian Duck": return BirdType.HawaiianDuck;
+            case "Kalij Pheasant": return BirdType.KalijPheasant;
+            case "Redfowl": return BirdType.RedFowl;
+            case "Iiwi": return BirdType.HoneyCreeper;
             default: return BirdType.None;
         }
     }
